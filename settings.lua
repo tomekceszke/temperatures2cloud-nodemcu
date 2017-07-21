9b54d13890fc569060abf0035ce4de8eec15efba
tomekceszke <tomekceszke@users.noreply.github.com>
2017-07-21 21:53:02 +0200

    changed interval

--- settings.lua
+++ settings.lua
@@ -8,6 +8,6 @@ pin = 4 -- GPIO pin where DS18B20 sensors are connected
 
 -- format [id] = { update_interval_in_seconds, "ts mapping field" }
 known_sensors_settings = {
-    [1] = { 40, "field3" },
-    [2] = { 260, "field2" }
+    [1] = { 55, "field3" },
+    [2] = { 606, "field2" }
 }
